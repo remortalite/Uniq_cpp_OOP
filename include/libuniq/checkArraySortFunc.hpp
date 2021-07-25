64b07b40edf4fc988d5a09156decf5e5b9191ac4
Ignat Mora <morrig407@gmail.com>
2021-07-25 23:03:48 +0700

    Add description for checkArraySortFunc()

--- include/libuniq/checkArraySortFunc.hpp
+++ include/libuniq/checkArraySortFunc.hpp
@@ -5,6 +5,12 @@
 
 #include <functional>
 
+/*!
+ *	\brief Check if checkSum and runNumber criterias passed.
+ *
+ *  Checks on random array with 1000 elements (array.fillRand(1000) is used)
+ *  if checkSums are equal and runNumber is equal to 1 after sorting.
+ */
 void checkArraySortFunc(std::function<void(Arr &)>);
 
 #endif // #ifndef CHECKARRAYSORTFUNC_HPP
